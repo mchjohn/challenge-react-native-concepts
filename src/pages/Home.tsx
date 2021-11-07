7f433c24f2e9ab850620f2872adc8804a185c49c
Michel John <michel.santos@dataside.com.br>
2021-11-07 19:01:24 -0300

    challenge completion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, task]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }));
+    
+    const findTask = updatedTasks.find(task => task.id === id);
+    
+    if (!findTask) return;
+
+    findTask.done = !findTask.done;
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
